337af5b3d2a9f3c8f422c434d3845b2ee40da1be
Kevin Zheng <jiezheng2020@gmail.com>
2021-02-24 18:29:22 -0600

    phase 3

--- big-o-intro-project/03-two-sum.js
+++ big-o-intro-project/03-two-sum.js
@@ -15,28 +15,60 @@ const arr = [0, 7, 1, 5];
 //for Loop from 0, let difference = targetSum - left[i]
 //if (left.includes(difference)){return true}
 //if indexOf(difference)
-console.log(okayTwoSum1(arr, 6)); // => should be true
-console.log(okayTwoSum1(arr, 10)); // => should be false
 
 
-function okayTwoSum1(arr, targetSum) {
-  // Code goes here ...
-  let left = []
-  for(let i = 0; i < arr.length; i ++){
-    if(arr[i] <= targetSum ){
-      left.push(arr[i])
-    }
-  }
-  for(let i = 0; i < left.length; i++){
-    let difference = targetSum - left[i]
-    let temp = left
-    temp.splice(i, 1)
-    if(temp.includes(difference)){
-     return true;
-    }
-  }
-     return false
+const recurBSearchIdx = (nums, targetNum, offset = 0) => {
+  if (nums.length === 0)
+    return -1;
+
+  let mid = Math.floor(nums.length / 2);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid + 1);
+
+  if (targetNum < nums[mid])
+    return recurBSearchIdx(left, targetNum);
+  else if (targetNum > nums[mid])
+    return recurBSearchIdx(right, targetNum, mid + 1 + offset);
+  else
+    return mid + offset;
 }
+
+
+// function okayTwoSum1(arr, targetSum) {
+//   // Code goes here ...
+//   let left = []
+//   for(let i = 0; i < arr.length; i ++){
+//     if(arr[i] <= targetSum ){
+//       left.push(arr[i])
+//     }
+//   }
+
+//   for(let i = 0; i < left.length; i++){
+//     let difference = targetSum - left[i]
+//     let temp = left.slice(0,i).concat(left.slice(i+1))
+//     if(temp.includes(difference)){
+//      return true;
+//     }
+//   }
+//      return false
+// }
+
+
+
+function okayTwoSum1(arr, targetSum) {  //   O(n) O(nlogn)
+  for (let i = 0; i < arr.length; i++) {
+    let targetNum = targetSum - arr[i]
+    let searchArr = arr.slice(0, i).concat(arr.slice(i + 1))
+
+    if(recurBSearchIdx(searchArr, targetNum)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// console.log(okayTwoSum1(arr, 6)); // => should be true
+console.log(okayTwoSum1(arr, 10)); // => should be false
 
 
 function okayTwoSum2(arr, targetSum) {
