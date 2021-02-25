614cf32ca7089995355ca2a156d54dad4f2639a8
Qingwei <qchen6060@gmail.com>
2021-02-24 19:16:37 -0500

    twoSum1

--- big-o-intro-project/03-two-sum.js
+++ big-o-intro-project/03-two-sum.js
@@ -11,11 +11,31 @@ function badTwoSum(arr, targetSum) {
 
 const arr = [0, 7, 1, 5];
 // [0,1,5,7]
-console.log(badTwoSum(arr, 6)); // => should be true
-console.log(badTwoSum(arr, 10)); // => should be false
+//quickSort [0,1,5]
+//for Loop from 0, let difference = targetSum - left[i]
+//if (left.includes(difference)){return true}
+//if indexOf(difference)
+console.log(okayTwoSum1(arr, 6)); // => should be true
+console.log(okayTwoSum1(arr, 10)); // => should be false
+
 
 function okayTwoSum1(arr, targetSum) {
   // Code goes here ...
+  let left = []
+  for(let i = 0; i < arr.length; i ++){
+    if(arr[i] <= targetSum ){
+      left.push(arr[i])
+    }
+  }
+  for(let i = 0; i < left.length; i++){
+    let difference = targetSum - left[i]
+    let temp = left
+    temp.splice(i, 1)
+    if(temp.includes(difference)){
+     return true;
+    }
+  }
+     return false
 }
 
 
